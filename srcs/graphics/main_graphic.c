e84051071f118e0966b7fe570f1a4f7df728c4af
Jaume GARCIA <jagarcia@e3r8p20.42.fr>
2018-04-30 20:28:15 +0200

    saving progress in grafics

--- srcs/graphics/main_graphic.c
+++ srcs/graphics/main_graphic.c
@@ -6,7 +6,7 @@
 /*   By: jagarcia <jagarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/29 22:41:08 by jagarcia          #+#    #+#             */
-/*   Updated: 2018/04/30 19:35:54 by mrodrigu         ###   ########.fr       */
+/*   Updated: 2018/04/30 20:27:48 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 
 int		main(void)
 {
-	char *line;
-//	char buffer[1];
+	char	*line;
+	int		dim[2];
 	
 	ft_seek(STDIN_FILENO, 49 * 5);
+	if (get_next_line(STDIN_FILENO, &line) < 0)
+		ft_error(NULL);
+	dim[0] = ft_
 	while (get_next_line(STDIN_FILENO, &line))
 	{
-		ft_putchar('A');
+		ft_putchar('<');
 		ft_putstr(line);
-		ft_putchar('A');
+		ft_putchar('>');
 		ft_putchar('\n');
 		ft_strdel(&line);
 	}

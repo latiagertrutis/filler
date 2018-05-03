ceb51dd73be4383fb0e77d61f0801e2d3a64528e
Jaume GARCIA <jagarcia@e3r4p14.42.fr>
2018-05-03 14:56:38 +0200

    grafics

--- includes/graphics.h
+++ includes/graphics.h
@@ -6,7 +6,7 @@
 /*   By: jagarcia <jagarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/29 07:08:13 by jagarcia          #+#    #+#             */
-/*   Updated: 2018/05/03 11:56:19 by jagarcia         ###   ########.fr       */
+/*   Updated: 2018/05/03 14:56:31 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 # define RESOLUTION_X 1366
 # define RESOLUTION_Y 768
 # define ESC 53
+# define ENDIAN 0
+# define BITS 32
 
 typedef struct	s_params
 {
@@ -37,5 +39,7 @@ typedef struct	s_mlx
 }				t_mlx;
 
 void	ft_initialice(t_params **params);
+void	ft_print_map(t_mlx *mlx);
+char	*ft_get_addrs(t_mlx *mlx);
 
 #endif
--- srcs/graphics/ft_print_map.c
+++ srcs/graphics/ft_print_map.c
@@ -6,8 +6,28 @@
 /*   By: jagarcia <jagarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/02 07:51:31 by jagarcia          #+#    #+#             */
-/*   Updated: 2018/05/02 12:36:20 by jagarcia         ###   ########.fr       */
+/*   Updated: 2018/05/03 14:56:19 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
+
+void	ft_print_map(t_mlx *mlx)
+{
+	char	*addrs;
+	int		i;
+	
+	addrs = ft_get_addrs(mlx);
+	i = 0;
+	while (i < RESOLUTION_Y - MARGEN_Y * 2)
+	{
+		ft_line();
+		i += mlx->square[1];
+	}
+	i = 0;
+	while (i < RESOLUTION_X - MARGEN_X * 2)
+	{
+		ft_line();
+		i += mlx->square[0];
+	}
+}

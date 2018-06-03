a1a447a6e039d2969c3ff37393b9c7ef1567e99d
Mateo RODRIGUEZ <mrodrigu@e3r8p15.42.fr>
2018-06-03 22:27:04 +0200

    progress con relieve

--- srcs/graphics/ft_progress.c
+++ srcs/graphics/ft_progress.c
@@ -6,7 +6,7 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/03 21:02:08 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/06/03 21:33:21 by mrodrigu         ###   ########.fr       */
+/*   Updated: 2018/06/03 21:59:57 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,24 @@ static void			center_image(t_mlx *mlx, int width)
 	mlx_put_image_to_window(mlx->ptr, mlx->win, mlx->progress, MARGEN_X + dist_x, RESOLUTION_Y - MARGEN_Y - 100);
 }
 
+static int	choose_color(int i, int p1)
+{
+	if (i < 10)
+		return (p1 ? 0x0273ff : 0x900272);
+	else if (i < 20)
+		return (p1 ? 0x068bff : 0xaa0585);
+	else if (i < 30)
+		return (p1 ? 0x0aa3ff : 0xc00999);
+	else if (i < 70)
+		return (p1 ? 0x0fbcff : 0xda0ead);
+	else if (i < 80)
+		return (p1 ? 0x0aa3ff : 0xc00999);
+	else if (i < 90)
+		return (p1 ? 0x068bff : 0xaa0585);
+	else
+		return (p1 ? 0x0273ff : 0x900272);
+}
+
 void		ft_progress(t_mlx *mlx, int width, int p1, int p2)
 {
 	int i;
@@ -45,7 +63,7 @@ void		ft_progress(t_mlx *mlx, int width, int p1, int p2)
 		{
 			j = i * width;
 			while (j < percent + i * width)
-				img_addr[j++] = 0x14eefe;
+				img_addr[j++] = choose_color(i, p1);
 			i++;
 		}
 		lp[0]++;
@@ -53,12 +71,12 @@ void		ft_progress(t_mlx *mlx, int width, int p1, int p2)
 	else
 	{
 		i = 0;
-		percent = (width * (lp[1] + p2)) / (lp[0] + lp[1] + p2);
+		percent = width - (width * (lp[0] + p1)) / (lp[0] + lp[1] + p1);
 		while (i < 100)
 		{
 			j = (width - 1) + (i * width);
 			while (j > (width - percent + i * width))
-				img_addr[j--] = 0xff69b4;
+				img_addr[j--] = choose_color(i, p1);
 			i++;
 		}
 		lp[1]++;
